7a15793046996342738c94a16d5d398b2d432ac9
GlowingSocc <gsocc@trg0d.me>
2019-09-04 23:38:06 -0500

    [r]

--- src/main/java/space/pxls/util/UserDupeIPTask.java
+++ src/main/java/space/pxls/util/UserDupeIPTask.java
@@ -18,10 +18,10 @@ public class UserDupeIPTask implements Runnable {
     @Override
     public void run() {
         if (!App.getDatabase().hasUserFlaggedLastIPAlert(user.getId())) {
-            App.getLogger().debug(System.out.printf("User %d has not been flagged, running checks...%n", user.getId()));
+            System.out.printf("User %d has not been flagged, running checks...%n", user.getId());
             List<Integer> uids = App.getDatabase().getDupedUsers(channel.getSourceAddress().getHostName(), user.getId());
             if (uids != null && uids.size() > 0) {
-                App.getLogger().debug(System.out.printf("User %d has %d accounts, iterating and sending report%n", user.getId(), uids.size()));
+                System.out.printf("User %d has %d accounts, iterating and sending report%n", user.getId(), uids.size());
                 StringBuilder toReport = new StringBuilder(String.format("User has %d IP matches in the database. Matched accounts:", uids.size()));
                 for (int i = 0; i < uids.size(); i++) {
                     User fetched = App.getUserManager().getByID(uids.get(i));
@@ -31,10 +31,10 @@ public class UserDupeIPTask implements Runnable {
                         System.err.printf("    ID from database (%d) resulted in a null user lookup (triggered by UserDupeIP task for %s (ID: %d))%n", uids.get(i), user.getName(), user.getId());
                     }
                 }
-                App.getLogger().debug(System.out.printf("Creating report for user %d and exiting", user.getId()));
+                System.out.printf("Creating report for user %d and exiting", user.getId());
                 App.getDatabase().addServerReport(toReport.toString(), user.getId());
                 App.getDatabase().flagLastIPAlert(user.getId());
             }
-        } else App.getLogger().debug(System.out.printf("User %d has already been flagged. Skipping%n", user.getId()));
+        } else System.out.printf("User %d has already been flagged. Skipping%n", user.getId());
     }
 }
